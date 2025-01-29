04f6c99256e58cf4310167ea839820562f2928e0
Hussam <engraggad60@gmail.com>
2025-01-29 09:01:33 +0300

    Edit: C05 - ex01 in progess

--- C05/ex01/ft_recursive_factorial.c
+++ C05/ex01/ft_recursive_factorial.c
@@ -1,3 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hussam <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/29 08:44:54 by hussam            #+#    #+#             */
+/*   Updated: 2025/01/29 08:44:56 by hussam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
 /*
 Create a recursive function that returns the factorial of the number given as a parameter.
 
@@ -6,3 +20,20 @@ Create a recursive function that returns the factorial of the number given as a 
 •Here’s how it should be prototyped :
 int ft_recursive_factorial(int nb);
 */
+
+int ft_recursive_factorial(int nb);
+
+int ft_recursive_factorial(int nb)
+{
+	int i;
+
+	i = nb - 1;
+	if (nb < 0)
+		return (-1);
+	else if (nb == 0 || nb == 1)
+		return (1);
+	else
+	{
+		//nb *= i--;
+	}
+}
