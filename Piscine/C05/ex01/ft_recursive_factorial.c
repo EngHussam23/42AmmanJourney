53d91f9979cbb4d4d281318b6e460522c36045ae
Hussam <engraggad60@gmail.com>
2025-06-14 20:24:36 +0300

    Common Core Update

--- Piscine/C05/ex01/ft_recursive_factorial.c
+++ Piscine/C05/ex01/ft_recursive_factorial.c
@@ -29,7 +29,7 @@ int ft_recursive_factorial(int nb)
 
 	i = nb - 1;
 	if (nb < 0)
-		return (-1);
+		return (0);
 	else if (nb == 0 || nb == 1)
 		return (1);
 	else
---
+++ Piscine/Fun/msg.c
@@ -0,0 +1,11 @@
+// #include <gratitude_message.h>
+#include <stdio.h>
+
+void gratitude_message() {
+    printf("Hey there! 🫡\n\nI hope you are doing well...\nAnd I hope I didn't inturrupt you doing anything important...\n\nIf I may to take a moment of your time, I would like to extend my gratitude for your support and guidance during the Piscine that have been invaluable to me, and I am deeply grateful for everything you have done 👏.\n\nYour personality is truly admirable, and I am constantly inspired by all of your beautiful traits from your smart thinking, to dedication to kindness and more.\n\nIt was a tough journey during the Piscine but never been better with people like you, and I am more than happy about it and excited for the upcoming journey in the Common Core phase of 42 Amman.\n\nBefore I end this message I one more thing to add if I may, I'am genuinely interested in getting to know you better and learning more about what a wonderful person you are. Every detail about you fascinates me, and I admire you greatly.\n\n And at the end I just want to say thank you for taking the time to read this message.\nI really appreciate it! 🙏😊");
+}
+
+int main(void) {
+    gratitude_message();
+    return 0;
+}
