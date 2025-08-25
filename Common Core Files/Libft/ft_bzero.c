337030c9d3c44f9f6d4f5cd06eaad8a5d933072e
EngHussam23 <engraggad60@gmail.com>
2025-08-25 15:19:35 +0300

    edit: fixing my libft(substr)

--- Common Core Files/Libft/ft_bzero.c
+++ Common Core Files/Libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: halragga <halragga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 16:26:48 by halragga          #+#    #+#             */
-/*   Updated: 2025/08/21 14:52:12 by halragga         ###   ########.fr       */
+/*   Updated: 2025/08/25 13:22:42 by halragga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	ft_bzero(void *ptr, size_t size)
 {
-	size_t		i;
-	char		*cptr;
+	size_t	i;
+	char	*cptr;
 
 	i = 0;
-	cptr = ptr;
+	cptr = (char *)ptr;
 	while (i < size)
 	{
 		cptr[i] = '\0';
