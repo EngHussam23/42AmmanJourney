a2a9da8d40a128c51b8d023c9b533a7fae0c683e
EngHussam23 <engraggad60@gmail.com>
2025-09-06 13:34:45 +0300

    edit: Libft & Printf

--- Common Core Files/Printf/main.c
+++ Common Core Files/Printf/main.c
@@ -6,15 +6,16 @@
 /*   By: halragga <halragga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/31 17:16:08 by halragga          #+#    #+#             */
-/*   Updated: 2025/09/01 19:37:15 by halragga         ###   ########.fr       */
+/*   Updated: 2025/09/04 18:22:07 by halragga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "Libft/libft.h"
 #include <stdio.h>
 #include <string.h>
 
 int	main(void)
 {
+	printf("testing the printf return value: %d\n", printf("Hello World!\n"));
 	return (0);
 }
