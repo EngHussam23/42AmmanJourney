d3e45c9ae8040c9f61f6a16bf7c6952d448171e0
EngHussam23 <engraggad60@gmail.com>
2025-09-14 14:36:31 +0300

    feat: testing

--- Common Core Files/Printf/main.c
+++ Common Core Files/Printf/main.c
@@ -3,18 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: halragga <halragga@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hussam <hussam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 13:59:45 by halragga          #+#    #+#             */
-/*   Updated: 2025/09/13 18:15:05 by halragga         ###   ########.fr       */
+/*   Updated: 2025/09/14 14:26:48 by hussam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
+#include <limits.h>
 
 int	main(void)
 {
-	ft_printf("test: %x", -1);
+	int x = 42;
+	int *px = &x;
+	char *str = "hello";
+
+	ft_printf("%p\n", px);   // prints the actual address of x
+	ft_printf("%p\n", str);  // prints the address of the string
+	ft_printf("%p\n", (void *)(uintptr_t)-ULONG_MAX);  // huge hex value
 	return (0);
 }
