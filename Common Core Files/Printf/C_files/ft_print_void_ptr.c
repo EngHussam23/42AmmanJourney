f39c42a5d2cfe580ff05e9b5b706ea42241a745d
EngHussam23 <engraggad60@gmail.com>
2025-09-14 14:36:07 +0300

    fix: recursion

--- Common Core Files/Printf/C_files/ft_print_void_ptr.c
+++ Common Core Files/Printf/C_files/ft_print_void_ptr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_void_ptr.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: halragga <halragga@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hussam <hussam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 18:42:53 by halragga          #+#    #+#             */
-/*   Updated: 2025/09/13 18:07:40 by halragga         ###   ########.fr       */
+/*   Updated: 2025/09/14 14:04:49 by hussam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 static int	convert_and_write(uintptr_t ptr, int fd, int count)
 {
+	if (ptr >= 16)
+		convert_and_write(ptr / 16, count, fd);
 	if (ptr % 16 < 10)
 		count = ft_write_char('0' + (ptr % 16), fd, count);
 	else if (ptr % 16 >= 10)
 		count = ft_write_char('a' + ((ptr % 16) - 10), fd, count);
-	else
-		convert_and_write(ptr / 16, count, fd);
 	count++;
 	return (count);
 }
