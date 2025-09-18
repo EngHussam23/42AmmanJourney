85c02d0f636d4d3b2e5e7369de66c8a59fcdc976
EngHussam23 <engraggad60@gmail.com>
2025-09-18 18:42:33 +0300

    fix: update author information and remove unused functions in ft_get_next_line.c

--- Common Core Files/Get_Next_Line/ft_get_next_line.c
+++ Common Core Files/Get_Next_Line/ft_get_next_line.c
@@ -3,43 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_get_next_line.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: halragga <halragga@student.42amman.com>    +#+  +:+       +#+        */
+/*   By: halragga <halragga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 13:54:08 by halragga          #+#    #+#             */
-/*   Updated: 2025/09/18 18:08:19 by halragga         ###   ########.fr       */
+/*   Updated: 2025/09/18 18:42:23 by halragga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_get_next_line.h"
-
-static char	*trim_stash(char *stash, int trim_length)
-{
-	char	*trimmed;
-
-	if (!stash[trim_length])
-	{
-		free(stash);
-		return (NULL);
-	}
-	trimmed = ft_substr(stash, trim_length, ft_strlen(stash) - trim_length);
-	free(stash);
-	return (trimmed);
-}
-
-static int	get_line_length(char *stash)
-{
-	int	count;
-
-	count = 0;
-	while (*stash != '\n' && *stash)
-	{
-		stash++;
-		count++;
-	}
-	if (*stash == '\n')
-		count++;
-	return (count);
-}
 
 char	*ft_get_next_line(int fd)
 {
