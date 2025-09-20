6d4f52bec11cd76468fd66f215c43ddf8ecad363
EngHussam23 <engraggad60@gmail.com>
2025-09-20 21:04:10 +0300

    eadit: comments

--- Common Core Files/Get_Next_Line/get_next_line.c
+++ Common Core Files/Get_Next_Line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: halragga <halragga@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 13:54:08 by halragga          #+#    #+#             */
-/*   Updated: 2025/09/20 20:53:25 by halragga         ###   ########.fr       */
+/*   Updated: 2025/09/20 21:01:47 by halragga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,17 @@ char	*ft_get_next_line(int fd)
 	int			nbytes;
 	char		*line;
 	char		*buff;
-	static char* stash;
+	static char	*stash;
 
 	buff = malloc(BUFFER_SIZE + 1);
-	read(fd, buff, BUFFER_SIZE);
+	nbytes = read(fd, buff, BUFFER_SIZE);
 	buff[BUFFER_SIZE] = '\0';
 	if (ft_strchr(buff, '\n'))
 	{
-		ft_strlcat(stash, buff, BUFFER_SIZE);
-		line = buff;
+		//push the buff by...
+		ft_strlcat(stash, ft_strchr(buff, '\n'), BUFFER_SIZE);
+		//push the buff by...
+		line = buff;//not correct
 		free(buff);
 		return (line);
 	}
