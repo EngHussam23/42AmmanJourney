0492bf7b7802ae66a1e8f4d9a8154a923f0f79a1
EngHussam23 <engraggad60@gmail.com>
2025-09-24 19:34:13 +0300

    Refactor get_next_line by removing stash_read function and updating extract_line logic

--- Common_Core_Files/Get_Next_Line/get_next_line.c
+++ Common_Core_Files/Get_Next_Line/get_next_line.c
@@ -6,36 +6,14 @@
 /*   By: halragga <halragga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 22:16:42 by halragga          #+#    #+#             */
-/*   Updated: 2025/09/24 18:14:18 by halragga         ###   ########.fr       */
+/*   Updated: 2025/09/24 19:33:31 by halragga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*stash_read(char *stash, char *buff, int nbytes)
-{
-	int		i;
-	char	*line;
-
-	i = -1;
-	stash = malloc(nbytes + 1);
-	if (!stash)
-		return (NULL);
-	while (++i < nbytes && buff[i] != '\n')
-		stash[i] = buff[i];
-	stash[i] = '\n';
-	concatinate(line, stash, i);
-	line[i + 1] = '\0';
-	while (++i < nbytes)
-	{
-		stash[i] = buff[i];
-	}
-	return (line);
-}
-
 static char	*extract_line(char **stash)
 {
-	
 }
 
 char	*get_next_line(int fd)
@@ -63,22 +41,5 @@ char	*get_next_line(int fd)
 		}
 	}
 	free(buff);
-	return ();
+	return (extract_line(&stash));
 }
-
-/*
-	nbytes = read(fd, buff, BUFFER_SIZE);
-	if (nbytes <= 0)
-	{
-		free(buff);
-		free(stash);
-		return (NULL);
-	}
-	else
-	{
-		buff[nbytes] = '\0';
-		line = stash_read(stash, buff, nbytes);
-		free(buff);
-	}
-	return (line);
-*/
