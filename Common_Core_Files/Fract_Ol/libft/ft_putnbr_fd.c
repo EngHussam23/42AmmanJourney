3f6d636848d073fa714112f0ed15cbcceda3da61
EngHussam23 <engraggad60@gmail.com>
2025-11-29 17:15:54 +0300

    feat: Libft

---
+++ Common_Core_Files/Fract_Ol/libft/ft_putnbr_fd.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: halragga <halragga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 17:41:34 by halragga          #+#    #+#             */
+/*   Updated: 2025/08/20 18:11:41 by halragga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	c;
+
+	c = '0';
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else if (n < 0)
+	{
+		write(fd, "-", 1);
+		ft_putnbr_fd(-n, fd);
+	}
+	else if (n == 0)
+		write(fd, "0", 1);
+	else if (n == 2147483647)
+		write(fd, "2147483647", 10);
+	else if (n > 0 && n < 10)
+	{
+		c += n;
+		write(fd, &c, 1);
+	}
+	else
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+}
