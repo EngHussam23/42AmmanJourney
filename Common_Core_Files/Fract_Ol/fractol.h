4fac2b38e189cd2a9aef6ba36d31ddc9a4101d84
EngHussam23 <engraggad60@gmail.com>
2025-11-30 14:36:17 +0300

    edit:  defines

--- Common_Core_Files/Fract_Ol/fractol.h
+++ Common_Core_Files/Fract_Ol/fractol.h
@@ -6,10 +6,26 @@
 /*   By: halragga <halragga@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 16:52:21 by halragga          #+#    #+#             */
-/*   Updated: 2025/11/29 17:04:08 by halragga         ###   ########.fr       */
+/*   Updated: 2025/11/30 13:55:50 by halragga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
+
+# define WIDTH 800
+# define HEIGHT 800
+# define MANDELBROT 1
+# define JULIA 2
+# define KEY_ESC 65307
+
+/*Standard Includes*/
+# include <mlx.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <math.h>
+# include <sys/time.h>
+
 #endif
