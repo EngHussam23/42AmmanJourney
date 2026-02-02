771cc5873e3e559f2a9f00426ed9c14cdeb34646
EngHussam23 <engraggad60@gmail.com>
2026-02-02 18:47:06 +0300

    feat: client side prepped

---
+++ common_core_files/mini_talk/mini_talk.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mini_talk.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: halragga <halragga@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/02 14:58:22 by halragga          #+#    #+#             */
+/*   Updated: 2026/02/02 17:55:57 by halragga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINI_TALK_H
+// # define _POSIX_C_SOURCE 200809L
+# define _XOPEN_SOURCE 700
+# define MINI_TALK_H
+
+// Standard libraries (allowed functions)
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+
+
+// My headers:
+# include "libft.h"
+
+#endif
